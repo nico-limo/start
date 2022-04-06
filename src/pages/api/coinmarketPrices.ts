e6f16d304aeb741c0340983e827ce0aa052377fb
Nico Limo <nicolas.limonoff@gmail.com>
2022-04-06 11:47:08 -0300

    deleted cors

--- src/pages/api/coinmarketPrices.ts
+++ src/pages/api/coinmarketPrices.ts
@@ -1,24 +1,6 @@
 import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { API_COINMARKET } from "../../utils/constants";
-import Cors from "cors";
-
-// Initializing the cors middleware
-const cors = Cors({
-  methods: ["GET", "HEAD"],
-});
-
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result);
-      }
-
-      return resolve(result);
-    });
-  });
-}
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -31,7 +13,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   };
 
   try {
-    await runMiddleware(req, res, cors);
     const { data: coinmarketData } = await axios.get(`${API_COINMARKET}`, {
       params,
       headers,
