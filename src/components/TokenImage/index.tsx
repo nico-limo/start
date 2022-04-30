ae5b06ba245d74eb54454c19fdbd78f3bac42e8f
Nico Limo <nicolas.limonoff@gmail.com>
2022-04-30 18:25:40 -0300

    dale

--- src/components/TokenImage/index.tsx
+++ src/components/TokenImage/index.tsx
@@ -6,7 +6,7 @@ const TokenImage = ({ symbol, w = 25, h = 25 }: TokenImageProps) => {
   const [imgSrc, setImgSrc] = useState(symbol);
   return (
     <Image
-      src={`/tokens/${imgSrc.toLowerCase()}.png`}
+      src={`/tokens/${imgSrc}.png`}
       alt={imgSrc}
       width={w}
       height={h}
